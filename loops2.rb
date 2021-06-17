918a4ac05a3f1354a2fc8e331ac0f081f183a95b
Carlos Aguirre <craguirre952@gmail.com>
2021-06-16 22:41:50 -0400

    init

---
+++ loops2.rb
@@ -0,0 +1,77 @@
+# 1. Write a while loop to print the numbers 1 through 10.
+
+# number = 1
+# while number <= 10
+#   p number
+#   number += 1
+# end
+
+
+# 2. Write a while loop that prints the word "hello" 5 times.
+
+# number = 1
+# while number <= 5
+#   p "hello"
+#   number += 1
+# end
+
+
+# 3. Write a while loop that asks the user to enter a word and will run forever until the user enters the word "stop".
+
+# print "Enter as many words as you want. Type 'stop' when done. "
+# while true
+#   response = gets.chomp
+#   if response == "stop"
+#     break
+#   end
+# end
+
+
+# # 4. Write a while loop that prints the numbers 0 through 100, increasing by 5 each time.
+
+# number = 0
+# while number <= 100
+#   p number
+#   number += 5
+# end
+
+
+# 5. Write a while loop that prints the number 9000 ten times.
+
+# number = 1
+# while number <= 10
+#   p 9000
+#   number += 1
+# end
+
+
+# 6. Write a while loop that asks the user to enter a number and will run forever until the user enters a number greater than 10.
+
+# while true
+#   print "Enter a random number "
+#   number = gets.chomp.to_i
+#   if number > 10
+#     p "that's it!"
+#     break
+#   else
+#     p number
+#   end
+# end
+
+
+# # 7. Write a while loop that prints the numbers 50 to 70.
+
+# number = 50
+# while number <= 70
+#   p number
+#   number += 1
+# end
+
+
+# # 8. Write a while loop that prints the phrase "Around the world" 144 times.
+
+# number = 0
+# while number < 144
+#   p "Around the World"
+#   number += 1
+# end
